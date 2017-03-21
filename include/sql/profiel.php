052221ff427c30910ed4ac442e2a27974f15425f
aldert otten <aldert otten>
2017-03-21 18:33:17 +0100

    een begin gemaakt met het profiel en een kleine wijziging bij template.php

---
+++ include/sql/profiel.php
@@ -0,0 +1,14 @@
+<?php
+class profile {
+	function __construct($conn) {
+		$this->conn = $conn;
+	}
+	/* ophalen van profielgegevens */
+	function profiled($username, $pass) {
+		/* geen idee waar ik mee bezig ben */
+	}
+
+	
+}
+
+$profile = new profile($connect->conn);
